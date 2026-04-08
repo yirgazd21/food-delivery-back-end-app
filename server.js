513fbbde13c5f48415a695bc56324fdccf2ca3e1
yirgazd21 <yirgalemzegeye2129.com>
2026-04-09 01:26:07 +0300

    server updated to file ssytem for image uploads in categoryController and server.js, and client updated to use environment variable for API URL and added styling to admin pages.

--- server.js
+++ server.js
@@ -30,11 +30,11 @@ app.use(cors({
 
 app.use(express.json());
 
-// const fs = require('fs');
-// const uploadDir = 'uploads';
-// if (!fs.existsSync(uploadDir)) {
-//     fs.mkdirSync(uploadDir, { recursive: true });
-// }
+const fs = require('fs');
+const uploadDir = 'uploads';
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 // Serve static files (uploaded images)
 app.use('/uploads', express.static('uploads'));
